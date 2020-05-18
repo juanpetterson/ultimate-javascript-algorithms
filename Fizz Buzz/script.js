f227ced2a7f703b57d011a31589f5bef36fdce48
juanpetterson <juan.petterson@gmail.com>
2020-05-17 22:37:42 -0300

    Adjusted the logs of fizzbuzz function

--- Fizz Buzz/script.js
+++ Fizz Buzz/script.js
@@ -2,11 +2,11 @@ function fizzBuzz(num) {
   for (i = 1; i <= num; i++) {
     // if (i % 3 === 0 && i % 5 === 0) console.log('FizzBuzz');
     // because to be divisible by 3 and 5 need to be multiple of 15, what is 3 * 5
-    if (i % 15 === 0) console.log('FizzBuzz');
-    else if (i % 3 === 0) console.log('Fizz');
-    else if (i % 5 === 0) console.log('Buzz');
+    if (i % 15 === 0) console.log('FizzBuzz', i);
+    else if (i % 3 === 0) console.log('Fizz', i);
+    else if (i % 5 === 0) console.log('Buzz', i);
     else console.log(i);
   }
 }
 
-console.log(fizzBuzz(30));
+fizzBuzz(30);
